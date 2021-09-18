0098088acfd4a887ac18dd15800b3144780c60ae
zissan.chowdhury@yahoo.com <zissan.chowdhury@yahoo.com>
2021-09-18 12:57:58 +0530

    Added styles package to product workspace

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,3 +1,3 @@
 const components = require("@product/components");
-
+require("@product/styles/dist/css/index.css");
 console.log(components.test());
---
+++ packages/components/dist/index.js
@@ -0,0 +1,5 @@
+const test = () => {
+  console.log("Hello, from components!!!");
+};
+
+module.exports = { test };
