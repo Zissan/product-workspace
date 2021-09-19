d444d80dc35b84ace7bf09d510e56bc994fb4b18
zissan.chowdhury@yahoo.com <zissan.chowdhury@yahoo.com>
2021-09-19 14:03:10 +0530

    added eslint, prettier

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,13 +1,13 @@
-import { render } from "react-dom";
+import { render } from 'react-dom';
 
-import "@product/styles/scss/index.scss";
-import "./styles/index.css";
+import '@product/styles/scss/index.scss';
+import './styles/index.css';
 
 render(
   <div>
-    <div className="header p-6">
+    <div className='header p-6'>
       <h1>Market</h1>
     </div>
   </div>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
